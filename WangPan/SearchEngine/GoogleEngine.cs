61026a264836359d27d7f911cc22ec245998d516
460791814 <460791814@qq.com>
2016-03-13 23:45:18 +0800

    架构搭建

---
+++ WangPan/SearchEngine/GoogleEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WangPan.Interface;
+
+namespace WangPan.SearchEngine
+{
+    public class GoogleEngine : ISearchEngine
+    {
+
+    }
+}
